31df1faf5dce0b167ca6f2e465293070be3828cb
ZhijieFan <1640410188@qq.com>
2024-10-17 21:29:44 +0800

    change parameter

--- segmentation/graph_cut.py
+++ segmentation/graph_cut.py
@@ -1,9 +1,15 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.abspath(os.path.join(current_dir, '..')))
+from utils import *
 
 
-def graph_cut(image_path):
+def graph_cut(image_path, iteration=5):
     # 读取图像
     image = cv2.imread(image_path)
     mask = np.zeros(image.shape[:2], np.uint8)  # 创建一个与图像大小相同的掩码
@@ -14,10 +20,10 @@ def graph_cut(image_path):
 
     # 定义矩形区域 (x, y, width, height) - 根据需要调整
     height, width = image.shape[:2]
-    rect = (width // 4, height // 4, width // 2, height // 2)
-
+    rect = (int(width / 4), int(height / 4), int(width / 1.2),
+            int(height / 1.2))
     # 应用 GrabCut 算法
-    cv2.grabCut(image, mask, rect, bgd_model, fgd_model, 5,
+    cv2.grabCut(image, mask, rect, bgd_model, fgd_model, iteration,
                 cv2.GC_INIT_WITH_RECT)
 
     # 将掩码转换为二进制图像
@@ -32,7 +38,7 @@ def graph_cut(image_path):
 if __name__ == "__main__":
     # 示例用法
     mask2, segmented_image, image = graph_cut(
-        '/home/fanzhijie/code/homework/medical_image_process/image/Data/Image/ISIC_0000028.png'
+        '/home/fanzhijie/code/homework/medical_image_process/image/Data/Image/ISIC_0000043.png'
     )  # 替换为你的图像路径
     # 显示原始图像和分割结果
     plt.figure(figsize=(10, 5))
